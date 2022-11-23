10ed53ec9791e11e878a6e6f74e560191fe15c93
Wei-Jan Chang <cc9868422@gmail.com>
2022-11-23 01:28:34 +0000

    note

--- mph/notebook/util.py
+++ mph/notebook/util.py
@@ -1,3 +1,4 @@
+#util,utility
 from pathlib import Path
 from typing import Optional, List
 
@@ -5,12 +6,12 @@ import pandas as pd
 
 from who_membership import WHO_MEMBERSHIP
 
-__all__ = ['select_df']
+__all__ = ['select_df'] #__ __ python習慣用法
 
 
 def select_df(df: pd.DataFrame,
               column_drop: Optional[List[str]] = None,
-              year: int = 2000,
+              year: int = 2000, #default 2000
               save_path: Optional[Path] = None) -> pd.DataFrame:
     """
     TODO write by yourself xxx
@@ -24,9 +25,11 @@ def select_df(df: pd.DataFrame,
     if column_drop is not None:
         df = df.drop(columns=column_drop)
 
-    year_mask = df['Year'] > year
-    entity_mask = [country in WHO_MEMBERSHIP for country in df['Entity']]
-    ret = df[year_mask & entity_mask].reset_index(drop=True)
+    year_mask: pd.Series[bool] = df['Year'] > year #type is list of bool; 比較df["year"]有沒有> default 的year
+    entity_mask :List[bool]= [country in WHO_MEMBERSHIP for country in df['Entity']] 
+        #在df裡Entity 裡的country有沒有也在who membership 裡，有的就True.
+    ret:pd.DataFrame = df[year_mask & entity_mask].reset_index(drop=True) 
+        #使用新的索引 df.reset_index; 把drop掉的也不要加進去新df裡(drop=True), ret=return
 
     if save_path is not None:
         ret.to_csv(save_path)
