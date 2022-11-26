6be8066af5c6c85666cda71d4d79036dbf52a59b
Yu Ting Wei <ytsimon2004@gmail.com>
2022-11-26 01:37:08 +0100

    doc, dropna

--- mph_wyt/notebook/util.py
+++ mph_wyt/notebook/util.py
@@ -7,35 +7,46 @@ import pandas as pd
 from who_membership import WHO_MEMBERSHIP
 
 __all__ = ['select_df']
+
+
 # __ __ python習慣用法
 
 
 def select_df(df: pd.DataFrame,
               column_drop: Optional[List[str]] = None,
-              # 預設沒有要drop掉column, 所以=None
               year: int = 2000,
-              # default 2000.
-              save_path: Optional[Path] = None) -> pd.DataFrame:
+              save_path: Optional[Path] = None,
+              drop_na: Optional[List[str]] = None) -> pd.DataFrame:
     """
-    TODO write by yourself xxx
-    :param df:
-    :param column_drop:
-    :param year:
-    :param save_path:
+    dataframe modification and save as another file
+
+    :param df: input dataframe
+    :param column_drop: drop the column(s) that are not informative
+    :param year: pick up the data that larger than which year
+    :param save_path: path for saving the modified dataframe
+    :param drop_na: specify if drop the na-existing column name
+
     :return:
     """
     df = df.copy()
     if column_drop is not None:
         df = df.drop(columns=column_drop)
 
-    year_mask: pd.Series[bool] = df['Year'] > year
     # type is list of bool; 比較df["year"]有沒有> default 的year
-    entity_mask :List[bool]= [country in WHO_MEMBERSHIP for country in df['Entity']] 
+    year_mask: pd.Series[bool] = df['Year'] > year
     # 在df裡Entity 裡的country有沒有也在who membership 裡，有的就True.
-    ret:pd.DataFrame = df[year_mask & entity_mask].reset_index(drop=True) 
+    entity_mask: List[bool] = [country in WHO_MEMBERSHIP for country in df['Entity']]
     # 使用新的索引 df.reset_index; 把drop掉的也不要加進去新df裡(drop=True), ret=return
+    _df: pd.DataFrame = df[year_mask & entity_mask].reset_index(drop=True)
+
+    if drop_na is not None:
+        try:
+            _df.dropna(subset=drop_na, inplace=True)
+        except KeyError as e:
+            raise ValueError(f'{e} not in the dataframe, should be one of the {_df.columns.tolist()}')
+
 
     if save_path is not None:
-        ret.to_csv(save_path)
+        _df.to_csv(save_path)
 
-    return ret
+    return _df
