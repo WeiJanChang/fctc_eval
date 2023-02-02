55141ce38c94e75191c23c0e996214e78c5502b3
Wei-Jan Chang <cc9868422@gmail.com>
2023-02-02 00:12:16 +0000

    def ratified parties

--- src/WHOFCTC_parties_date.py
+++ src/WHOFCTC_parties_date.py
@@ -29,15 +29,28 @@ def format_date(df: pd.DataFrame,
     return df
 
 
+def ratified_parties(df: pd.DataFrame,
+                     drop_na: None,
+                     save_path: Optional[Path] = None) -> pd.DataFrame:
+    if drop_na is not None:
+        df = df.mask(df['Entity'].isin(['Argentina', 'Cuba', 'Switzerland']), np.nan)
+        df.dropna(subset=['Entity'], inplace=True)
+        df = df.dropna(how='any')
+    if save_path is not None:
+        df.to_excel(save_path)
+    return df
+
+
 if __name__ == '__main__':
     df = pd.read_excel('/Users/wei/Python/MPHDissertation/test_file/WHO_FCTC_Parties_date_filter .xlsx')
     rename = {
         "Ratification, Acceptance(A), Approval(AA), Formal confirmation(c), Accession(a), Succession(d)": 'Ratification'}
     uniform_date = ['Signature', 'Ratification']
     # save_path = '/Users/wei/UCD-MPH/MPH-Lecture:Modules/MPH Dissertation/Data/WHOFCTC_Parties_date_filter.xlsx'
-    # format_date(df, rename_mapping=rename, uniform_date=uniform_date, save_path=save_path)
-    df1 = pd.read_excel('/Users/wei/Python/MPHDissertation/test_file/Final_CVD_Tobacco_merged.xlsx')
+    format_date(df, rename_mapping=rename, uniform_date=uniform_date)
 
-    entity_ratified = df1.mask(df1['Entity'].isin(['Argentina', 'Cuba', 'Switzerland']), np.nan)
-    entity_ratified.dropna(subset=['Entity'], inplace=True)
-    # entity_ratified.to_excel('entity_ratified_test.xlsx')
+    df1 = pd.read_excel('/Users/wei/Python/MPHDissertation/test_file/CVD_Tobacco_merged_without_missingdata.xlsx')
+    drop_na = True
+    save_path = '/Users/wei/Python/MPHDissertation/src/test_ratifies.xlsx'
+    ratified_parties(df1, drop_na=drop_na, save_path=save_path)
+
