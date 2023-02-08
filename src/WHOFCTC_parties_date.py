6a5c3778ede713549038d9e60e6403d0ff0f7d79
Wei-Jan Chang <cc9868422@gmail.com>
2023-02-08 01:41:59 +0000

    drop index

--- src/WHOFCTC_parties_date.py
+++ src/WHOFCTC_parties_date.py
@@ -31,11 +31,15 @@ def format_date(df: pd.DataFrame,
 
 def ratified_parties(df: pd.DataFrame,
                      drop_na: None,
+                     column_drop: Optional[List[str]] = None,
                      save_path: Optional[Path] = None) -> pd.DataFrame:
     if drop_na is not None:
-        df = df.mask(df['Entity'].isin(['Argentina', 'Cuba', 'Switzerland']), np.nan)
-        df.dropna(subset=['Entity'], inplace=True)
+        df = df.mask(df['Country Name'].isin(['Argentina', 'Cuba', 'Switzerland']), np.nan)
+        df.dropna(subset=['Country Name'], inplace=True)
         df = df.dropna(how='any')
+    if column_drop is not None:
+        df = df.drop(columns=column_drop)
+
     if save_path is not None:
         df.to_excel(save_path)
     return df
@@ -46,11 +50,12 @@ if __name__ == '__main__':
     rename = {
         "Ratification, Acceptance(A), Approval(AA), Formal confirmation(c), Accession(a), Succession(d)": 'Ratification'}
     uniform_date = ['Signature', 'Ratification']
-    # save_path = '/Users/wei/UCD-MPH/MPH-Lecture:Modules/MPH Dissertation/Data/WHOFCTC_Parties_date_filter.xlsx'
-    format_date(df, rename_mapping=rename, uniform_date=uniform_date)
+    save_path = '/Users/wei/UCD-MPH/MPH-Lecture:Modules/MPH Dissertation/Data/WHOFCTC_Parties_date_filter.xlsx'
+    format_date(df, rename_mapping=rename, uniform_date=uniform_date, save_path=save_path)
 
-    df1 = pd.read_excel('/Users/wei/Python/MPHDissertation/test_file/CVD_Tobacco_merged_without_missingdata.xlsx')
+    df1 = pd.read_excel(
+        '/Users/wei/UCD-MPH/MPH-Lecture:Modules/MPH Dissertation/Data/merge_cvd_tobacco_no_missingdata.xlsx')
     drop_na = True
-    save_path = '/Users/wei/Python/MPHDissertation/src/test_ratifies.xlsx'
-    ratified_parties(df1, drop_na=drop_na, save_path=save_path)
-
+    col_drop = ['Unnamed: 0.1', 'Unnamed: 0']
+    save_path = '/Users/wei/UCD-MPH/MPH-Lecture:Modules/MPH Dissertation/Data/WHOFCTC_Parties_date_ratified.xlsx'
+    ratified_parties(df1, drop_na=drop_na, column_drop=col_drop, save_path=save_path)
