dfccbaa5f6c11411a63416f87f6ed49c2ee09ebf
Wei-Jan Chang <cc9868422@gmail.com>
2023-05-27 18:36:29 +0100

    create world map, install package

---
+++ src/map.py
@@ -0,0 +1,12 @@
+"""
+import subprocess
+
+# Install GeoPandas
+subprocess.check_call(["pip", "install", "geopandas"])
+
+#
+subprocess.check_call(["pip", "install", "numpy"])
+subprocess.check_call(["pip", "install", "pandas"])
+subprocess.check_call(["pip", "install", "matplotlib"])
+subprocess.check_call(["pip", "install", "geopy"])
+"""
