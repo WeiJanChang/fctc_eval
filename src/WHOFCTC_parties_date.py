4e5511e9ff5a370fc15c6ad6de782e32a207c1df
Wei-Jan Chang <98678790+WeiJanChang@users.noreply.github.com>
2024-10-21 14:08:20 +0100

    docstring; merge_df function

--- src/WHOFCTC_parties_date.py
+++ src/WHOFCTC_parties_date.py
@@ -2,47 +2,50 @@
 pipeline
 
 Step 1:
-
         Download signature and ratification of FCTC raw data from UN
         https://treaties.un.org/pages/ViewDetails.aspx?src=TREATY&mtdsg_no=IX-4&chapter=9&clang=_en
-
-
 Step 2:
-
         format date 'dd/mm/yy'
         header:['Country Name', 'Signature', 'Ratification']
         **188 (188, 3)**
-
 Step 3:
-
         merge WHOFCTC_Parties_date_formatted.xlsx (n=188) and merge_cvd_tobacco.xlsx (n=89)
         drop_missing_data
         **82 (424, 22)**
-
 step 4 :
-
         mask non-ratified parties
         **78 (400, 22)**
-
-
 """
 
 import pandas as pd
-from typing import Optional, List, Dict
+from typing import Optional, List, Dict, Union
 from pathlib import Path
 import numpy as np
+
+PathLike = Union[Path, str]
 
 
 def format_date(df: pd.DataFrame,
                 rename_mapping: Dict[str, str] = None,
                 formatted_date: Optional[List[str]] = None,
                 save_path: Optional[Path] = None) -> pd.DataFrame:
+    """
+
+    :param df: Signatures and Ratifications- UN Treaty Section_08 Feb_2023_Name chnaged.xlsx
+    :param rename_mapping: {
+    'Participant': 'Country Name',
+    "Ratification, Acceptance(A), Approval(AA), Formal confirmation(c), Accession(a), Succession(d)": 'Ratification'
+    }
+    :param formatted_date: ['Signature', 'Ratification']
+    :param save_path: save as WHOFCTC_Parties_date_formatted.xlsx
+    :return: df
+    """
     if rename_mapping is not None:
         df = df.rename(columns=rename_mapping)
     if formatted_date is not None:
         for col in formatted_date:
-            #df[col] = pd.to_datetime(df[col], infer_datetime_format=True)
-            #df[col] = pd.to_datetime(df[col].dt.strftime("%d %b %Y").dt.strftime("%Y"))
+            # df[col] = pd.to_datetime(df[col], infer_datetime_format=True)
+            # df[col] = pd.to_datetime(df[col].dt.strftime("%d %b %Y").dt.strftime("%Y"))
             df[col] = pd.to_datetime(df[col], infer_datetime_format=True).dt.strftime("%d %b %Y").str.extract(
                 r'(\d{4})')
 
@@ -81,44 +84,22 @@ def final_selected(df: pd.DataFrame,
     return df
 
 
-if __name__ == '__main__':
-    df = pd.read_excel(
-        '/Users/wei/UCD-MPH/MPH Lecture/MPH Dissertation/Data (WHO CVD and Tobacco Use)/raw data/Signatures and Ratifications- UN '
-        'Treaty Section_08 Feb_2023_Name chnaged.xlsx',
-        engine='openpyxl')
-    rename = {'Participant': 'Country Name',
-              "Ratification, Acceptance(A), Approval(AA), Formal confirmation(c), Accession(a), Succession(d)": 'Ratification'}
-    formatted_date = ['Signature', 'Ratification']
-    save_path = '/Users/wei/UCD-MPH/MPH Lecture/MPH Dissertation/Data (WHO CVD and Tobacco Use)/WHOFCTC_Parties_date_formatted.xlsx'
-    df = format_date(df, rename_mapping=rename, formatted_date=formatted_date, save_path=save_path)
-    # merge
-    df1 = pd.read_excel(
-        '/Users/wei/UCD-MPH/MPH Lecture/MPH Dissertation/Data (WHO CVD and Tobacco Use)/cvd_tobacco_nomissingdata.xlsx',
-        engine='openpyxl')
+def merge_df(df1: pd.DataFrame, df2: pd.DataFrame, drop_na: bool = False,
+             merge_output: PathLike = None) -> pd.DataFrame:
+    """
 
-    df2 = pd.read_excel(
-        '/Users/wei/UCD-MPH/MPH Lecture/MPH Dissertation/Data (WHO CVD and Tobacco Use)/WHOFCTC_Parties_date_formatted.xlsx',
-        engine='openpyxl')
+    :param df: df1-cvd_tobacco_nomissingdata.xlsx; df2-WHOFCTC_Parties_date_formatted.xlsx
+    :param merge_output:
+    :return:
+    """
 
-signed_df = pd.merge(df1, df2, on=['Country Name'], how='outer')
-signed_df.fillna(value='NaN', inplace=True)
-signed_df.to_excel('/Users/wei/UCD-MPH/MPH Lecture/MPH '
-                   'Dissertation/Data (WHO CVD and Tobacco Use)/WHOFCTC_Parties_signed_date.xlsx')
+    signed_df = pd.merge(df1, df2, on=['Country Name'], how='outer')
+    signed_df.fillna(value='NaN', inplace=True)
+    if drop_na:
+        signed_df.replace("NaN", np.nan, inplace=True)
+        signed_df = signed_df.dropna(axis=0)
 
-# drop_missing_data
-signed_df.replace("NaN", np.nan, inplace=True)
-signed_df= signed_df.dropna(axis=0)
+    if merge_output is not None:
+        signed_df.to_excel('WHOFCTC_Parties_signed_date.xlsx', index=False)
 
-signed_df.to_excel(
-    "/Users/wei/UCD-MPH/MPH Lecture/MPH Dissertation/Data (WHO CVD and Tobacco Use)/WHOFCTC_Parties_date_no_missingdata.xlsx")
-
-# 19 countries selected
-
-result_df = signed_df[signed_df.isin(
-    ['Estonia', 'Costa Rica', 'Mexico', 'Czechia', 'Netherlands', 'Georgia', 'Spain', 'Singapore', 'Latvia', 'Germany',
-     'Guatemala', 'Kazakhstan', 'Austria', 'Serbia', 'Lithuania', 'Ecuador', 'Iceland', 'Slovenia', 'Mauritius',
-     ]).any(axis=1)].dropna(how='all')
-
-result_df.to_excel("/Users/wei/UCD-MPH/MPH Lecture/MPH Dissertation/Data (WHO CVD and Tobacco Use)/19_ratified_country.xlsx")
-
-
+    return signed_df
